c8c65c7bce6f34929bb2eb5d0b481d47cb4d97e1
Alexandra Zamfirescu <alexandra.zamfirescu1@s.unibuc.ro>
2025-01-09 02:00:25 +0200

    authorize groups + messages done
    - added UserGroups DbSet
    - many to many relationship

--- Controllers/GroupsController.cs
+++ Controllers/GroupsController.cs
@@ -1,5 +1,6 @@
 ﻿using Connectify.Data;
 using Connectify.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -27,46 +28,145 @@ namespace Connectify.Controllers
         }
 
         //index - afisarea tuturor grupurilor
+        [Authorize(Roles = "User,Admin")]
         public IActionResult Index()
         {
-            var groups = dbc.Groups;
+            // preluarea tuturor grupurilor
+            var groups = dbc.Groups.Include(g=>g.UserGroups).ToList();
+            var currentUserId = _userManager.GetUserId(User);
 
-            ViewBag.Groups = groups;
+            // grupurile din care face parte utilizatorul curent
+            var userGroups = dbc.UserGroups
+                .Where(ug => ug.UserId == currentUserId)
+                .Select(ug => ug.GroupId)
+                .ToList();
+
+            // adaugarea datelor in ViewBag
+            ViewBag.UserGroups = userGroups.Where(id => id.HasValue)
+                .Select(id => id.Value)
+                .ToList();                            // id-urile grupurilor la care userul are acces
+            ViewBag.CurrentUserId = currentUserId;     
+            ViewBag.Groups = groups;                  // toate grupurile
+
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.Message = TempData["message"];
+            }
 
             return View();
+        }
+
+
+
+        // functie de verificare daca un user face parte dintr-un grup
+        public bool IsUserInGroup(int groupId, string userId)
+        {
+            return dbc.UserGroups.Any(ug => ug.GroupId == groupId && ug.UserId == userId);
+        }
+
+
+        // show - afisarea unui grup dupa id cu mesajele asociate
+        [Authorize(Roles = "User,Admin")]
+        public ActionResult Show(int id)
+        {
+            var currentUserId = _userManager.GetUserId(User); // id-ul utilizatorului curent pentru a-l putea folosi in view
+
+            // daca userul e admin sau face parte din grupul respectiv
+            if (User.IsInRole("Admin") || IsUserInGroup(id, currentUserId))
+            {
+                Group group = dbc.Groups.Include(g => g.Messages).FirstOrDefault(g => g.Id == id);
+                if (group != null)
+                {
+                    ViewBag.Group = group;
+                    ViewBag.CurrentUserId = currentUserId;
+                    return View(group);
+                }
+                else
+                {
+                    return StatusCode(404); // not found
+                }
+            }
+            else
+            {
+                return StatusCode(403); // acces interzis
+            }
         }
 
         // show - afisarea unui grup dupa id cu mesajele asociate
-        public ActionResult Show(int id)
+        [HttpPost]
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult Show(int id, [FromForm] Message message)
         {
-            Group group = dbc.Groups.Include(g => g.Messages)
-                .Where(g => g.Id == id)
-                .FirstOrDefault();
-
-            if (group == null)
+            var currentUserId = _userManager.GetUserId(User); // id-ul utilizatorului curent pentru a-l putea folosi in view
+            // daca userul e admin sau face parte din grupul respectiv
+            if (User.IsInRole("Admin") || IsUserInGroup(id, currentUserId))
             {
-                return NotFound();
-            }
+                message.SentAt = DateTime.Now;
+                message.GroupId = id;
+                message.UserId = currentUserId;
 
-            ViewBag.Group = group;
+                // daca mesajul este valid
+                if (ModelState.IsValid)
+                {
+                    dbc.Messages.Add(message);
+                    dbc.SaveChanges();
+                    TempData["message"] = "Message added successfully!";
+                    return Redirect("/Groups/Show/" + message.GroupId);
+                }
+                else
+                {
+                    Group group = dbc.Groups.Include("Messages")
+                        .Include(Message => Message.User)
+                        .Where(g => g.Id == id)
+                        .FirstOrDefault(g => g.Id == id);
+
+                    return View(group);
+                }
+            }
+            else
+            {
+                return StatusCode(403); // acces interzis
+            }
+        }
+
+
+
+
+
+        // New - formular pentru crearea unui grup
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult New()
+        {
+            Group group = new Group();
             return View(group);
         }
 
-        // New - formular pentru crearea unui grup
-        public IActionResult New()
-        {
-            return View();
-        }
+
 
         [HttpPost]
+        [Authorize(Roles = "User,Admin")]
         public IActionResult New(Group group)
         {
+            // userul care creeaza grupul va fi salvat in baza de date
+            var currentUserId = _userManager.GetUserId(User);
+            group.UserId = currentUserId;
+
             try
             {
                 group.GroupName = group.GroupName;
                 group.Description = group.Description;
 
                 dbc.Groups.Add(group);
+                dbc.SaveChanges();
+
+                // crearea unei inregistrari in UserGroup pentru utilizatorul curent
+                var userGroup = new UserGroup
+                {
+                    UserId = currentUserId,
+                    GroupId = group.Id
+                };
+
+                dbc.UserGroups.Add(userGroup);
                 dbc.SaveChanges();
 
                 return RedirectToAction("Index");
@@ -77,66 +177,117 @@ namespace Connectify.Controllers
             }
         }
 
+
         // edit
-        // httpget default
+        [Authorize(Roles = "User,Admin")]
         public IActionResult Edit(int id)
         {
-            Group? group = dbc.Groups.Find(id);
+            Group? group = dbc.Groups.Where(g => g.Id == id).FirstOrDefault(); // cautam grupul dupa id
 
-            if (group == null)
+            // daca userul e admin sau a creat grupul respectiv, poate sa-l editeze
+            if (User.IsInRole("Admin") || group?.UserId == _userManager.GetUserId(User))
             {
-                return NotFound();
+                if (group != null)
+                {
+                    return View(group);
+                }
+                else
+                {
+                    return StatusCode(404); // not found
+                }
             }
-
-            ViewBag.Group = group;
-            return View(group);
+            else
+            {
+                return StatusCode(403); // acces interzis
+            }
         }
 
         // formularul de editare
         [HttpPost]
+        [Authorize(Roles = "User,Admin")]
         public IActionResult Edit(int id, Group requestGroup)
         {
-            Group? group = dbc.Groups.Find(id);
+                Group? group = dbc.Groups.Find(id);
 
-            if (group == null)
-            {
-                return NotFound();
-            }
+                if (group != null)
+                {
+                    if (User.IsInRole("Admin") || group.UserId == _userManager.GetUserId(User))
+                    {
+                        group.GroupName = requestGroup.GroupName;
+                        group.Description = requestGroup.Description;
 
-            try
-            {
-                group.GroupName = requestGroup.GroupName;
-                group.Description = requestGroup.Description;
-                TempData["message"] = "Group updated successfully!";
-                dbc.SaveChanges();
-
-                return RedirectToAction("Index");
-            }
-            catch (Exception) // cand nu se poate face update
-            {
-                TempData["message"] = "Group not found!";
-                return RedirectToAction("Edit", new { id = group.Id });
-            }
+                        dbc.SaveChanges();
+                        TempData["message"] = "Group updated successfully!";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return StatusCode(403); // forbidden
+                }
+                }
+                return StatusCode(404); // not found
         }
 
-        // delete - stergerea unui grup
-        [HttpPost]
+            // delete - stergerea unui grup
+            // adminul si cel care a creat grupul pot sa-l stearga
+            [HttpPost]
+        [Authorize(Roles = "User,Admin")]
         public ActionResult Delete(int id)
         {
             Group? group = dbc.Groups.Include(g => g.Messages)
                         .FirstOrDefault(g => g.Id == id);
 
-
-            if (group != null)
+            // daca userul e admin sau a creat grupul respectiv, poate sa-l stearga
+            if (User.IsInRole("Admin") || group?.UserId == _userManager.GetUserId(User))
             {
-                dbc.Groups.Remove(group);
-                dbc.SaveChanges();
-                return RedirectToAction("Index");
+                if (group != null)
+                {
+                    dbc.Groups.Remove(group);
+                    dbc.SaveChanges();
+                    TempData["message"] = "Group deleted successfully!";
+                    TempData["messageType"] = "alert-danger";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return StatusCode(404); // not found
+                }
             }
             else
             {
-                return StatusCode(StatusCodes.Status404NotFound);
+                return StatusCode(403); // acces interzis
             }
+        }
+
+
+
+        // FUNCTIONALITATI SPECIFICE 
+        // join group
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult JoinGroup(int id)
+        {
+            var currentUserId = _userManager.GetUserId(User);
+            // daca userul nu face parte din grup
+            if (!IsUserInGroup(id, currentUserId))
+            {
+                var userGroup = new UserGroup
+                {
+                    UserId = currentUserId,
+                    GroupId = id
+                };
+                dbc.UserGroups.Add(userGroup);
+                dbc.SaveChanges();
+                TempData["message"] = "You have joined the group!";
+                TempData["messageType"] = "alert-success";
+            }
+
+            else
+            {
+                TempData["message"] = "You are already in this group!";
+                TempData["messageType"] = "alert-danger";
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.CompilerServices;
 
 // PASUL 1: useri si roluri
@@ -13,6 +15,30 @@ namespace Connectify.Models
         public string? Bio { get; set; }
         //public string ProfilePicture { get; set; }
         public bool IsPrivate { get; set; } // daca profilul este privat sau nu
+
+        // un user poate avea mai multe postari
+        public ICollection<Post>? Posts { get; set; }
+
+        // un user poate avea mai multe comentarii
+        public ICollection<Comment>? Comments { get; set; }
+
+        // un user poate trimite mai multe mesaje 
+        public ICollection<Message>? Messages { get; set; }
+
+        // un user poate face parte din mai multe grupuri
+        public ICollection<UserGroup>? UserGroups { get; set; }
+
+        // un user poate trimite si primi mai multe cereri de follow
+        // public ICollection<FollowRequest>? SentFollowRequests { get; set; }
+        // public ICollection<FollowRequest>? ReceivedFollowRequests { get; set; }
+
+
+        // pentru selectarea rolurilor dintr-un dropdown
+        [NotMapped]
+        public IEnumerable<SelectListItem>? Roles { get; set; } 
+
+
+
 
     }
 }
